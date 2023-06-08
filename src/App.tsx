8780d314db524d8be821bcd27e932a63aefee520
Nikita Karangle <nikita.karangle@beyondid.com>
2023-06-08 13:36:42 +0530

    Display User on click Refresh button

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function App() {
+interface User {
+  name: {
+    title: string;
+    first: string;
+    last: string;
+  };
+  email: string;
+}
+
+const App: React.FC = () => {
+  const [user, setUser] = useState<User | null>(null);
+
+  const fetchUser = async () => {
+    try {
+      const response = await axios.get('https://randomuser.me/api');
+      const userData = response.data.results[0];
+      const { name, email } = userData;
+      const user = {
+        name: {
+          title: name.title,
+          first: name.first,
+          last: name.last,
+        },
+        email,
+      };
+      setUser(user);
+      localStorage.setItem('user', JSON.stringify(user));
+    } catch (error) {
+      console.error('Error fetching user:', error);
+    }
+  };
+
+  useEffect(() => {
+    const storedUser = localStorage.getItem('user');
+    if (storedUser) {
+      setUser(JSON.parse(storedUser));
+    } else {
+      fetchUser();
+    }
+  }, []);
+
+  const handleRefresh = () => {
+    fetchUser();
+  };
+
+  if (!user) {
+    return <div>Loading...</div>;
+  }
+
+  const { name, email } = user;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{`${name.title} ${name.first} ${name.last}`}</h1>
+      <p>{email}</p>
+      <button onClick={handleRefresh}>Refresh</button>
     </div>
   );
-}
+};
 
 export default App;
